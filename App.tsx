128c7d978bd50573394ebf3a469ec3243a8d3b20
Eduardo Sousa <eduardobrunoit@gmail.com>
2023-09-20 20:43:58 -0400

    feat add atypes for styled componnets

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { Groups } from '@screens/Groups';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components/native';
 
-import themes from './src/themes';
+import theme from './src/theme';
 
 export default function App() {
   return (
-    <ThemeProvider theme={themes}>
+    <ThemeProvider theme={theme}>
       <Groups />
     </ThemeProvider>
   );
