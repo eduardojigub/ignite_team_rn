f3a180c80351d27a94528fb8232318eb4ce0209a
Eduardo Sousa <eduardobrunoit@gmail.com>
2023-09-20 20:33:18 -0400

    feat: add theme provider

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
 import { Groups } from '@screens/Groups';
+import { ThemeProvider } from 'styled-components';
+
+import themes from './src/themes';
 
 export default function App() {
-  return <Groups />;
+  return (
+    <ThemeProvider theme={themes}>
+      <Groups />
+    </ThemeProvider>
+  );
 }
