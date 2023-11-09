3165bcbf3ccc06d5401ad67b3247a1e6198e7ff9
Eduardo Sousa <eduardobrunoit@gmail.com>
2023-11-09 11:49:53 -0500

    feat: add navigation types

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,10 +6,17 @@ import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
 
+import { useNavigation } from '@react-navigation/native';
+
 import { Container } from './styles';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
+
+  const navigation = useNavigation();
+  function handleNewGroup() {
+    navigation.navigate('new');
+  }
 
   return (
     <Container>
@@ -25,7 +32,7 @@ export function Groups() {
         )}
         showsVerticalScrollIndicator={false}
       />
-      <Button title="Cadastrar turma" />
+      <Button title="Cadastrar turma" onPress={handleNewGroup} />
     </Container>
   );
 }
