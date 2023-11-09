776b8b248c49c380e86e6650d6f90e6807823cb1
Eduardo Sousa <eduardobrunoit@gmail.com>
2023-11-08 21:34:02 -0500

    feat: create FlatList

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,17 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
 import { ButtonIcon } from '@components/ButtonIcon';
-import { Container, Form } from './styles';
+import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
 import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
 
 export const Players = () => {
+  const [team, setTeam] = useState('Time a');
+  const [players, setPlayers] = useState(['Bruno', 'João', 'Maria']);
+
   return (
     <Container>
       <Header showBackButton />
@@ -18,7 +24,21 @@ export const Players = () => {
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
-      <Filter title="Time A" />
+      <HeaderList>
+        <FlatList
+          data={['Time a', 'Time b']}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
     </Container>
   );
 };
