c25f5ebf24e037ebd685e12450ac24eecff90b43
Eduardo Sousa <eduardobrunoit@gmail.com>
2023-11-09 11:45:50 -0500

    fix: version package react navigation

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import {
 
 import theme from './src/theme';
 
-import { Groups } from '@screens/Groups';
+import { Routes } from './src/routes';
 import { Loading } from '@components/Loading';
 
 export default function App() {
@@ -22,7 +22,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
