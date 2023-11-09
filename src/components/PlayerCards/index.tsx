2784e697182b1ca6252e3489bb2ce8dc6c7e59c4
Eduardo Sousa <eduardobrunoit@gmail.com>
2023-11-08 21:51:31 -0500

    feat: create PlayerCards

---
+++ src/components/PlayerCards/index.tsx
@@ -0,0 +1,17 @@
+import { Container, Icon, PlayerName } from './styles';
+import { ButtonIcon } from '@components/ButtonIcon';
+
+type Props = {
+  name: string;
+  onRemove: () => void;
+};
+
+export function PlayerCard({ name, onRemove }: Props) {
+  return (
+    <Container>
+      <Icon name="person" />
+      <PlayerName>{name}</PlayerName>
+      <ButtonIcon icon="close" type="SECONDARY" onPress={onRemove} />
+    </Container>
+  );
+}
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,14 +3,18 @@ import { FlatList } from 'react-native';
 
 import { Header } from '@components/Header';
 import { ButtonIcon } from '@components/ButtonIcon';
-import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
 import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
+import { PlayerCard } from '@components/PlayerCards';
+
+import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export const Players = () => {
   const [team, setTeam] = useState('Time a');
-  const [players, setPlayers] = useState(['Bruno', 'João', 'Maria']);
+  const [players, setPlayers] = useState([]);
 
   return (
     <Container>
@@ -39,6 +43,23 @@ export const Players = () => {
         />
         <NumberOfPlayers>{players.length}</NumberOfPlayers>
       </HeaderList>
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time" />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          !players.length && { flex: 1 },
+        ]}
+      />
+
+      <Button title="Remover Turma" type="SECONDARY" />
     </Container>
   );
 };
