06015eac7a46aca59599fe3cf9aef5b488102062
Eduardo Sousa <eduardobrunoit@gmail.com>
2023-11-08 19:45:15 -0500

    feat: create dynamic button

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,8 @@
 import { Header } from '@components/Header';
-import { Container } from './styles';
+import { ButtonIcon } from '@components/ButtonIcon';
+import { Container, Form } from './styles';
 import { Highlight } from '@components/Highlight';
+import { Input } from '@components/Input';
 
 export const Players = () => {
   return (
@@ -11,6 +13,10 @@ export const Players = () => {
         title="Nome da Turma"
         subtitle="Adicione a galera e separe os times"
       />
+      <Form>
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+        <ButtonIcon icon="add" />
+      </Form>
     </Container>
   );
 };
